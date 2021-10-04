7299bedb1105351dee0d5e976b031465947aba78
ArnabRoger <arnab0227@gmail.com>
2021-10-05 00:26:19 +0530

    Aded gh pages

--- src/components/Slidedata.js
+++ src/components/Slidedata.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export const Slidedata =[
     {
         image: '/OBT/images/front-view.jpg'
@@ -21,28 +21,4 @@ export const Slidedata =[
     {
         image: '/OBT/images/purulia.jpg'
     },
-=======
-export const Slidedata =[
-    {
-        image: '/OBT/images/front-view.jpg'
-    },
-    {
-        image: '/OBT/images/Ahaldara.jpeg'
-    },
-    {
-        image: '/OBT/images/darjeeling.jpg'
-    },
-    {
-        image: '/OBT/images/cherapunji.jpg'
-    },
-    {
-        image: '/OBT/images/dooars.jpg'
-    },
-    {
-        image: '/OBT/images/meghalaya1.jpg'
-    },
-    {
-        image: '/OBT/images/purulia.jpg'
-    },
->>>>>>> 7fa9c3a2a57225bd7af0ee5e336da5de0842f1ae
-];
+]
